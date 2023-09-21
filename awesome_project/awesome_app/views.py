a91500fee57bd0b2f61a41aaab63147376e9a49b
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-21 14:15:30 +0900

    feat: add functions

--- awesome_project/awesome_app/views.py
+++ awesome_project/awesome_app/views.py
@@ -2,3 +2,33 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'awesome_app/main.html')
+
+def login(request):
+    return render(request, 'registration/login.html', )
+
+def logout(request):
+    logout(request)
+    return render(request, "awesome_app/main.html")
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def trade(request):
+    return render(request, 'awesome_app/trade.html')
+
+def trade_post(request):
+    return render(request, 'awesome_app/trade_post.html')
+
+def write(request):
+    return render(request, 'awesome_app/write.html')
+
+def search(request):
+    posts = []
+    return render(request, 'awesome_app/search.html', {"posts": posts})
+
+def location(request):
+    return render(request, 'awesome_app/location.html')
+
+def chat(request):
+    region = ''
+    return render(request, 'awesome_app/chat.html', {"region" : region})
