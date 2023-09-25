1afbc31127baf54bfa287a7ef1611ad50a486bed
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-25 15:48:49 +0900

    feat: add custom form

---
+++ awesome_project/awesome_app/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+class CustomLoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력해주세요', 'class': 'login-input'}),
+        label='아이디',
+        label_suffix='', 
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호',
+        label_suffix='', 
+    )
+
+class CustomRegistrationForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '아이디를 입력해주세요', 'class': 'login-input'}),
+        label='아이디',
+        label_suffix='', 
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호',
+        label_suffix='', 
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': '비밀번호를 다시 입력해주세요', 'class': 'login-input'}),
+        label='비밀번호 확인',
+        label_suffix='', 
+    )
