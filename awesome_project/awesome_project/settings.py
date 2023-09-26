706b43c30bde6d938462bde2dff02dace9307a62
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-26 11:14:22 +0900

    feat: add media path

--- awesome_project/awesome_project/settings.py
+++ awesome_project/awesome_project/settings.py
@@ -135,3 +135,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'awesome_app/', STATIC_URL)
+MEDIA_URL = '/'
