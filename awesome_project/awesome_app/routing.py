a77c4f29c8ed0f35ee22161d0f2addd3c5258c0c
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-27 11:35:32 +0900

    fix: modify variable

--- awesome_project/awesome_app/routing.py
+++ awesome_project/awesome_app/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import customers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\d+)/$", customers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<room_number>\d+)/$", customers.ChatConsumer.as_asgi()),
 ]
 
