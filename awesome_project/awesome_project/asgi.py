57bbbc74b1a8531f2ef0bfcc7b41dd68fa02a691
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-27 09:37:51 +0900

    feat: add websocket setting

--- awesome_project/awesome_project/asgi.py
+++ awesome_project/awesome_project/asgi.py
@@ -1,22 +1,13 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import awesome_app.routing 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awesome_project.settings')
 
-import awesome_app.routing
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(awesome_app.routing.websocket_urlpatterns))
-        ),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        awesome_app.routing.websocket_urlpatterns,
+    ),
+})
